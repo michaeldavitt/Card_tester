e13252ff4c03975206b9d7787760145b15abff7d
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2023-03-12 11:29:31 +0000

    Adds notes on how the script will be structured

--- card_tester.py
+++ card_tester.py
@@ -1,2 +1,8 @@
 # Purpose of this script is to simulate card testing
 # Scenario - I've purchased some stolen credit card creds from the dark web and a leaked Stripe API key, and want to test if the cards will get declined or not
+# Generate the card numbers
+# Replace the card numbers with Stripe test cards
+# Plug the card creds into the payment methods API
+# Generate the customer objects
+# Attach the payment method objects to the customer objects
+# Store the successful card numbers for future use, and discard the rest
