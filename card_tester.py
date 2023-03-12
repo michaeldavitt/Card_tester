051f4a30247253e7e17ae0a0bcaaf8a9f9cc2072
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2023-03-12 17:36:52 +0000

    Creates functions for generating random card creds

--- card_tester.py
+++ card_tester.py
@@ -8,12 +8,29 @@
 # Store the successful card numbers for future use, and discard the rest
 
 import stripe
+import random
 
 with open("keys.txt") as f:
     PASSWORD = ''.join(f.readlines())
     stripe.api_key = str(PASSWORD).split()[0]
 
-starter_subscription = stripe.Product.create(
-    name="Starter Subscription",
-    description="$12/Month subscription",
-)
+# Generate the card numbers/CVC
+
+
+def random_with_N_digits(n):
+    """Accepts an integer n and returns a random number where length = integer"""
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return random.randint(range_start, range_end)
+
+# Generate expiry
+
+
+def random_expiry(n):
+    """Accepts an integer n and returns an expiry date where month is between 1 and 12 and year is between next year and next year + n"""
+    month_list = list(range(1, 13))
+    month = random.choice(month_list)
+    next_year = 24
+    year_list = list(range(next_year, next_year + n + 1))
+    year = random.choice(year_list)
+    return month, year
