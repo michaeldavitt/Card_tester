480b1320ff8cb85925b53e55f249c81ad6e1c520
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2023-03-12 17:20:34 +0000

    Imports stripe, reads in API keys, makes test call

--- card_tester.py
+++ card_tester.py
@@ -6,3 +6,14 @@
 # Generate the customer objects
 # Attach the payment method objects to the customer objects
 # Store the successful card numbers for future use, and discard the rest
+
+import stripe
+
+with open("keys.txt") as f:
+    PASSWORD = ''.join(f.readlines())
+    stripe.api_key = str(PASSWORD).split()[0]
+
+starter_subscription = stripe.Product.create(
+    name="Starter Subscription",
+    description="$12/Month subscription",
+)
