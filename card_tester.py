10ef9dce25566e147e066fc30076c5e24f6048b9
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2023-03-12 20:49:55 +0000

    Adds progress bar and comments

--- card_tester.py
+++ card_tester.py
@@ -7,11 +7,13 @@
 # Attach the payment method objects to the customer objects
 # Store the successful card numbers for future use, and discard the rest
 
-from base64 import encode
 import stripe
 import random
 import csv
+from progress.bar import IncrementalBar
 
+
+# Read in Stripe API key
 with open("keys.txt") as f:
     PASSWORD = ''.join(f.readlines())
     stripe.api_key = str(PASSWORD).split()[0]
@@ -38,26 +40,41 @@ def random_expiry(n):
     return month, year
 
 
-N = 10
-creds = []
+# Number of credentials we want to generate
+N = 100
+
+# Stripe test cards (randomly generated 16-digit cards won't work in the testmode API)
 successful_card = "4242424242424242"
 declined_card = "4000000000000002"
 
+# List to store card number, expiry and CVC
+creds = []
+
+# Loop to generate N sets of credentials
 for i in range(N):
+    # Generate random 16 digit card number
     rand_card_num = random_with_N_digits(16)
 
-    # replace card number with stripe test card
+    # Generate stripe test card
     if rand_card_num % 10 == 0:
         rand_card_num_test = successful_card
     else:
         rand_card_num_test = declined_card
 
+    # Generate random expiry and CVC
     rand_exp = random_expiry(10)
     rand_cvc = random_with_N_digits(3)
+
+    # Add credentials to credential list
     creds.append([str(rand_card_num), rand_card_num_test,
                  rand_exp[0], rand_exp[1], rand_cvc])
 
 # Plug the card creds into the payment methods API
+
+# Progress bar
+bar = IncrementalBar('Creating Payment Methods', max=N)
+
+# Call payments method API N times using the credentials from the previous code block
 payment_methods = []
 for i in range(N):
     pm_object = stripe.PaymentMethod.create(
@@ -72,8 +89,15 @@ for i in range(N):
 
     payment_methods.append(pm_object.id)
 
+    bar.next()
+
+bar.finish()
+print()
 
 # Generate the customer objects
+bar = IncrementalBar('Creating Customers', max=N)
+
+# Call the customers API N times
 customers = []
 for i in range(N):
     cus_name = f"Customer #{i+1}"
@@ -81,31 +105,47 @@ for i in range(N):
     cus_object = stripe.Customer.create(name=cus_name, email=cus_email)
     customers.append(cus_object.id)
 
+    bar.next()
+
+bar.finish()
+print()
 
 # Attach the payment method objects to the customer objects
 successful_payment_methods = []
 for i in range(N):
+    # Attempt to attach payment method i to customer i
     try:
         stripe.PaymentMethod.attach(
             payment_methods[i],
             customer=customers[i],
         )
 
+    # Outcome: Card declined (throws 402 error - stripe.error.CardError)
     except stripe.error.CardError:
         print(
-            f"Failure! Card Number: {creds[i][0]} Card Expiry: {creds[i][2]}/{str(creds[i][3])[2:]} Card CVC: {creds[i][4]}")
+            f"Declined! Card Number: {creds[i][0]} Card Expiry: {creds[i][2]}/{str(creds[i][3])[2:]} Card CVC: {creds[i][4]}")
 
+    # Outcome: Card succeeded and was successfully attached to the customer object
     else:
         print(
-            f"Success! Card Number: {creds[i][0]} Card Expiry: {creds[i][2]}/{str(creds[i][3])[2:]} Card CVC: {creds[i][4]}")
+            f"Succeeded! Card Number: {creds[i][0]} Card Expiry: {creds[i][2]}/{str(creds[i][3])[2:]} Card CVC: {creds[i][4]}")
+
+        # Store successful card numbers
         successful_payment_methods.append(creds[i])
 
 
-# Store the successful card numbers for future use, and discard the rest
+# Export the successful card numbers to a CSV file for future use, and discard the rest
 header = ["card_number", "card_exp_month", "card_exp_year", "card_cvc"]
+
 with open("live_cards.csv", "w", encoding="UTF8", newline="") as f:
     writer = csv.writer(f)
+
+    # Adds header to CSV
     writer.writerow(header)
+
     for i in range(len(successful_payment_methods)):
+        # Removes the test card, since this is not useful
         successful_payment_methods[i].pop(1)
+
+        # Adds card credentials to next row in CSV file
         writer.writerow(successful_payment_methods[i])
