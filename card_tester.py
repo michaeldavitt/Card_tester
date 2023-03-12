b9ca144b70d5a38566a13ded24f26521bb3e2eac
Michael Davitt <63263218+michaeldavitt@users.noreply.github.com>
2023-03-12 11:15:42 +0000

    Initial Commit

---
+++ card_tester.py
@@ -0,0 +1,2 @@
+# Purpose of this script is to simulate card testing
+# Scenario - I've purchased some stolen credit card creds from the dark web and a leaked Stripe API key, and want to test if the cards will get declined or not
